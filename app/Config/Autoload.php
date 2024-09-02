8e4e50c399ae9796ed4e964deaedc14c53d99b3b
Ricopie <myiico.sorapps@hotmail>
2024-09-02 23:51:42 +0700

    Adding Modules Namespace

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -41,6 +41,7 @@ class Autoload extends AutoloadConfig
      */
     public $psr4 = [
         APP_NAMESPACE => APPPATH,
+        MODULES_NAMESPACE => APPPATH . 'Modules',
     ];
 
     /**
--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -92,3 +92,8 @@ define('EVENT_PRIORITY_NORMAL', 100);
  * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_HIGH instead.
  */
 define('EVENT_PRIORITY_HIGH', 10);
+
+/**
+ * Using for Modules in App
+ */
+define('MODULES_NAMESPACE', 'Modules');
